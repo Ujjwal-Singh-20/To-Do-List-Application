0bbdecfe7b6578eff9458b0b6f911df9b8ffecb5
Ujjwal Singh <ujjusingh20@gmail.com>
2024-07-16 16:23:17 +0530

    Adding function to delete item script.js

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ function addTodo() {
 
     const li = document.createElement('li');
     li.textContent = todoText;
-    document.getElementById('todo-list').appendChild(li);
 
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = function() {
+        this.parentElement.remove();
+    };
+    li.appendChild(deleteButton);
+
+    document.getElementById('todo-list').appendChild(li);
     document.getElementById('new-todo').value = '';
 }
+
 
 document.getElementById('todo-list').addEventListener('click', function(e) {
     if (e.target.tagName === 'LI') {
